b485fc78632607eb4886d4ca98634f39e30c3af9
Andrew Vineyard <dimensional42@gmail.com>
2017-11-04 19:43:56 +0000

    updated basic server

--- nodeproject/app.js
+++ nodeproject/app.js
@@ -1,2 +1,13 @@
-var aloha = "Hello Folks!";
-console.log(aloha);
+var express = require('express');
+var app = express();
+
+var port = process.env.PORT;
+//var port = 5000;
+
+app.get('/', function(req, res){
+    res.send('Hello Folks!');
+});
+
+app.listen(port, function(err){
+    console.log('Server is up and runnong on port: ' + port);
+});
